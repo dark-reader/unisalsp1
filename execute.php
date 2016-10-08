04c5e98c9fe0be4461f5701df0d69dc38ef8167a
Luke <dark-reader@users.noreply.github.com>
2016-10-08 17:22:21 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -20,7 +20,7 @@ $text = trim($text);
 $text = strtolower($text);
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters = array('chat_id' => $chatId, "lol" => $text);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
 

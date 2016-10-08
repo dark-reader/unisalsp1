92f96d1a2fc024f153cef29dbd6423513473e120
Luke <dark-reader@users.noreply.github.com>
2016-10-08 17:44:02 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -31,13 +31,7 @@ $parameters = array('chat_id' => $chatId, "text" => $text);
 // method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
 $parameters["method"] = "sendMessage";
 
-$response = '';
 
-
-if(strpos($text, "/start") === 0 || $text=="ciao")
-{
-	$response = "Ciao $firstname, benvenuto!";
-}
 // imposto la keyboard
 $parameters["reply_markup"] = '{ "keyboard": [["Orario lezioni (pdf)"], ["Orario lezioni (html)"], ["Elenco news"]], "one_time_keyboard": true}';
 // converto e stampo l'array JSON sulla response
